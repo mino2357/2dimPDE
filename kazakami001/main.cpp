f2a20bc1c4ac6b8e35fed1f9cba235955e9d4cb0
mino2357 <mino2357@gmail.com>
2018-01-11 15:18:34 +0900

    kazakami

---
+++ kazakami001/main.cpp
@@ -0,0 +1,182 @@
+/*
+ * guruguru test.
+ *
+ * kazakami method.
+ * 
+ * Takaaki MINOMO.
+ */
+
+#include <iostream>
+#include <cmath>
+#include <array>
+
+constexpr int N = 128;
+constexpr double Lx =  2.0;
+constexpr double Ly =  2.0;
+constexpr double xstart = - 1.0;
+constexpr double ystart = - 1.0;
+constexpr double dx = Lx / N;
+constexpr double dy = Ly / N;
+constexpr double dt = 0.0001;
+constexpr double pi = 3.14159265358979323846264338327950288;
+constexpr double tLimit = 20000.0 * pi;
+constexpr int INTV = 400;
+constexpr int plus = 4;
+
+namespace rittai3d{
+	namespace utility{
+		// [ minimum, maximum ) の範囲でラップアラウンド
+		template <typename T>
+		constexpr T wrap_around(T value, T minimum, T maximum){
+			const T n = (value - minimum) % (maximum - minimum);
+			return n >= 0 ? (n + minimum) : (n + maximum); 
+		}
+	}
+}
+
+namespace sksat {
+	template<std::size_t Num, typename T = double>
+	class array_wrapper{
+	private:
+		std::array<T, Num> arr;
+	public:
+		constexpr array_wrapper() : arr() {}
+		~array_wrapper() = default;
+
+		T& operator[](int i){
+			i = rittai3d::utility::wrap_around(i, 0, static_cast<int>(Num));
+			return arr[i];
+		}
+	};
+}
+
+using extendedArray = sksat::array_wrapper<N + 1, sksat::array_wrapper<N + 1>>;
+
+namespace mino2357{
+    
+    template <typename T = double>
+    void makeInitFuncF(extendedArray& f){
+        T x, y;
+        T a = 0.0; T b = 0.0;
+        for(int i=0; i<=N; ++i){
+            for(int j=0; j<=N; ++j){
+                x = xstart + i * dx;
+                y = ystart + j * dy;
+                
+                if(x >= 0.2 && x <= 0.6 && y >= 0.2 && y <= 0.6){
+                    f[i][j] = 1.0;
+                }else{
+                    f[i][j] = 0.0;
+                }
+                
+               //f[i][j] = //.1 * (std::sin(pi * (x)));// + std::sin(pi * (y));
+                       //0.1 * std::exp(- 20.0 * (std::pow(x - a, 2) + std::pow(y - b, 2)));
+                
+            } 
+        }
+    }
+
+    
+
+    class gnuplot{
+    private:
+        FILE* gp;
+    public:
+        gnuplot(){
+            gp = popen("gnuplot -persist", "w");
+            fprintf(gp, "set pm3d\n");
+            fprintf(gp, "set pm3d map\n");
+            fprintf(gp, "set contour\n");
+            fprintf(gp, "set xr [%f:%f]\n", xstart, xstart + Lx);
+            fprintf(gp, "set yr [%f:%f]\n", ystart, ystart + Ly);
+          	//fprintf(gp, "set zr [-0.1:1.1]\n");
+            fprintf(gp, "set size square\n");
+        }
+        ~gnuplot(){ pclose(gp);}
+
+        void print(extendedArray f){
+            fprintf(gp, "splot '-' w l\n");
+            for(int i=0; i<=N; i = i + plus){
+                for(int j=0; j<=N; j = j + plus){
+                    //fprintf(gp, "%f %f %f\n", xstart + i * dx, ystart + j * dy, mino2357::speed(i, j, u1, v1));
+                    fprintf(gp, "%f %f %f\n", xstart + i * dx, ystart + j * dy, f[i][j]);
+                }
+                fprintf(gp, "\n");
+            }
+            fprintf(gp, "e\n");
+            fflush(gp);
+        }
+    };
+
+    template <typename T = double>
+    T alpha(T x, T y){
+        return - y;
+    }
+    
+    template <typename T = double>
+    T beta(T x, T y){
+        return x;
+    }
+
+    template <typename T = double>
+    void succ(extendedArray& f1, extendedArray& f2){
+        T x, y;
+        for(int i=0; i<=N; ++i){
+            for(int j=0; j<=N; ++j){
+                x = xstart + i * dx;
+                y = ystart + j * dy;
+
+                if(alpha(x,y) >= 0.0 && beta(x,y) >= 0.0){
+                    f2[i][j] = f1[i][j] - alpha(x,y) * dt * (f1[i][j] - f1[i-1][j]) / dx
+                                        - beta(x,y)  * dt * (f1[i][j] - f1[i][j-1]) / dy;
+                }else if(alpha(x,y) < 0.0 && beta(x,y) >= 0.0){
+                    f2[i][j] = f1[i][j] + alpha(x,y) * dt * (f1[i][j] - f1[i+1][j]) / dx
+                                        - beta(x,y)  * dt * (f1[i][j] - f1[i][j-1]) / dy;
+                }else if(alpha(x,y) >= 0.0 && beta(x,y) < 0.0){
+                    f2[i][j] = f1[i][j] - alpha(x,y) * dt * (f1[i][j] - f1[i-1][j]) / dx
+                                        + beta(x,y)  * dt * (f1[i][j] - f1[i][j+1]) / dy;
+                }else{
+                    f2[i][j] = f1[i][j] + alpha(x,y) * dt * (f1[i][j] - f1[i+1][j]) / dx
+                                        + beta(x,y)  * dt * (f1[i][j] - f1[i][j+1]) / dy;
+                }
+            }
+        }
+    }
+
+}
+
+
+int main(){
+
+    auto f1 = extendedArray{};
+    auto f2 = extendedArray{};
+    
+    mino2357::makeInitFuncF(f1);
+
+    std::cout << dt / dx << std::endl;
+
+    auto gp = mino2357::gnuplot();    
+
+    gp.print(f1);
+    std::cout << "Enter!" << std::endl;
+    getchar();
+
+
+    double t = 0.0;
+
+    for(int it=0; t<=tLimit; ++it){
+
+        mino2357::succ(f1, f2);
+
+        if(it%INTV == 0){
+            gp.print(f2);
+        }
+   
+        for(int i=0; i<=N; ++i){
+            for(int j=0; j<=N; ++j){
+                f1[i][j] = f2[i][j];
+            }
+        }
+    }
+    gp.print(f1);
+}
